84a7cf03271951b8bbcdc9c754e74af2131b1166
Clarence <clarence.felicilda007@gmail.com>
2025-03-25 14:22:14 +0800

    umay

--- src/checkoutHeader.jsx
+++ src/checkoutHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './checkoutHeader.css';
 
-const CheckoutHeader = () => {
+const checkoutHeader = () => {
   return (
     <div className="checkout-header d-flex align-items-center p-3 bg-warning text-dark">
       <img src={`${process.env.PUBLIC_URL}/tcj_logo.png`} alt="TCJ Auto Supply" className="logo-image me-3" />
@@ -14,4 +14,4 @@ const CheckoutHeader = () => {
   );
 };
 
-export default CheckoutHeader;
+export default checkoutHeader;
